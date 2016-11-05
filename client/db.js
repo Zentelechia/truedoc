37329aca3647c138e5e519465f80377e1ba34b73
Yury <zentelechia@gmail.com>
2016-11-05 14:52:30 +0300

    .

--- client/db.js
+++
@@ -1,1 +0,0 @@
-var Docs = new FilesCollection({collectionName: 'Docs'});
---
+++ shared.js
@@ -0,0 +1,30 @@
+this.Docs = new FilesCollection({
+  collectionName: 'Docs',
+  storagePath: '~/data',
+  allowClientCode: false, // Disallow remove files from Client
+  onBeforeUpload: function (file) {
+    return true;
+    /*
+    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+      return true;
+    } else {
+      return 'Please upload image, with size equal or less than 10MB';
+    }
+    */
+  },
+  onAfterUpload: function(fileObj) {
+    Meteor.call("move2ipfs",fileObj);
+    /*
+    let reader = new FileReader();
+    reader.onload = function(){
+      let arrayBuffer = new Uint8Array(reader.result);
+
+      Meteor.call("ipfsAdd", arrayBuffer, function(err, result){
+        console.log("ipfs hash ",result);
+      });
+    }
+    reader.readAsArrayBuffer(fileObj);
+ */
+    }
+  
+});
