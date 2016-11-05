fe6745744b9c449b4b6a6f0330b54a1b1507dbbb
Yury <zentelechia@gmail.com>
2016-11-05 14:02:13 +0300

    .

--- server/methods.js
+++ server/methods.js
@@ -4,11 +4,11 @@ move2ipfs: function(fileObj){
 
   var fs = Npm.require('fs');
     // file originally saved as public/data/taxa.csv
-    fs.readFile(fileObj.path,  function (err, data) {
-        Meteor.call("ipfsAdd", data, function(err, result){
+    data=fs.readFileSync(fileObj.path);
+     
+     Meteor.call("ipfsAdd", data, function(err, result){
         console.log("ipfs hash ",result);
       });
     //    console.log(data);
-    });
-}
+} 
 });
