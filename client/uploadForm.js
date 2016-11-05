eb5bad149013877608a8e351675ad1dc6b537d60
Yury <zentelechia@gmail.com>
2016-11-05 13:26:04 +0300

    .

--- client/uploadForm.js
+++ client/uploadForm.js
@@ -2,11 +2,14 @@ var Docs = new FilesCollection({
   collectionName: 'Docs',
   allowClientCode: false, // Disallow remove files from Client
   onBeforeUpload: function (file) {
+    return true;
+    /*
     if (file.size <= 10485760 ){ // && /doc/rtfpng|jpg|jpeg/i.test(file.extension)) {
       return true;
     } else {
       return 'Please upload image, with size equal or less than 10MB';
     }
+    */
   }
 });
 
