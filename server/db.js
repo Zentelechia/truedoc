a6b9df7f4d9be529fbf84098bea55aedcaf3a2ce
Yury <zentelechia@gmail.com>
2016-11-05 13:09:58 +0300

    first

---
+++ server/db.js
@@ -0,0 +1,26 @@
+var Docs = new FilesCollection({
+  collectionName: 'Docs',
+  storagePath: '~/data',
+  allowClientCode: false, // Disallow remove files from Client
+  onBeforeUpload: function (file) {
+    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+      return true;
+    } else {
+      return 'Please upload image, with size equal or less than 10MB';
+    }
+  },
+  onAfterUpload: function(fileObj) {
+
+    let reader = new FileReader();
+    reader.onload = function(){
+      let arrayBuffer = new Uint8Array(reader.result);
+
+      Meteor.call("ipfsAdd", arrayBuffer, function(err, result){console.log("ipfs hash ",result);});
+    };
+    reader.readAsArrayBuffer(fileObj);
+
+
+});
+
+  }
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+// for global access on server side
+ipfsObj = false;
 
-Meteor.startup(() => {
-  // code to run on server at startup
+const testIpfs = function () {
+  // start ipfs daemon
+  let started = ipfsObj.start();
+  // wait for process to start
+  if (started) {
+    // test api calls https://www.npmjs.com/package/ipfs-api
+    ipfsObj.api.add(new Buffer('random stuff'), (err, data)=> {
+      console.log('ipfs hash ' + data[0].Hash);
+    });
+  }
+};
+
+Meteor.startup(function () {
+  ipfsObj          =  IpfsConnector.getInstance(); //singleton
+  ipfsObj.setLogLevel('info'); // info is default
+  testIpfs();
 });
