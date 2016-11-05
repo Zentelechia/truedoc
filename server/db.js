d6f54063c54f2ff0386e8e3e7955123b63e2ef2a
Yury <zentelechia@gmail.com>
2016-11-05 23:32:10 +0300

    .

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 Documents=new Meteor.Collection('documents');
 Meteor.publish("mydocs", function(user_id){
 	user=Meteor.users.findOne(user_id);
-	return Documents.find({$or: [{user_id: user._id},{to: user.emails.address}]});
+
+	return Documents.find({$or: [{user_id: user._id},{to: user.emails[0].address}]});
 })
